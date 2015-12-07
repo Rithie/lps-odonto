9374bcb7451a57a7f9bd1c4ade3cc8759f48a92d
Euler Alvarenga <euler.sistemas@gmail.com>
2015-12-07 13:24:24 -0200

    added class user

--- src/com/sparta/odonto/model/User.java
+++ src/com/sparta/odonto/model/User.java
@@ -11,4 +11,58 @@ package com.sparta.odonto.model;
  */
 public class User {
     
+    	private int id;
+	private String lastName;
+	private String firstName;
+	private String email;
+        private String password;
+
+    public User(int id, String lastName, String firstName, String email) {
+	
+	this.id = id;
+	this.lastName = lastName;
+	this.firstName = firstName;
+	this.email = email;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+            
+    
 }
