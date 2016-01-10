f83e9cd663e4eb57881a00c13fef9b7bb9f5b74b
Euler Alvarenga <euler.sistemas@gmail.com>
2016-01-09 22:03:54 -0200

    added Class Pessoa

---
+++ src/com/sparta/odonto/model/Pessoa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sparta.odonto.model;
+
+/**
+ *
+ * @author Rithie7
+ */
+public class Pessoa {
+
+	private Integer id_pessoa;
+  	private Integer endereco_id_endereco;
+  	private String nome;
+  	private String sobrenome;
+  	private String cpf;
+  	private String rg;
+  	private String email;
+  	private String data_nascimento;
+  	private String sexo;
+  	private String telefone;
+    
+}
